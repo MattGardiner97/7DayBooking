fd8cf642714671a79d66409ccea2b99447799a85
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-03-28 15:37:10 +1000

    Added Schedule migration amd model

---
+++ Website/app/Schedule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    protected $table = "Schedules";
+    public $timestamps = false;
+    
+    protected $fillable =[
+        "CounsellorID", "StartDate","EndDate","ScheduleString"
+    ];
+    protected $test;
+
+    protected $attributes =[
+                "StartDate" => "2020-01-01",
+        "EndDate" => "2021-01-01",
+        "ScheduleString" => ""
+    ];
+
+    public function GetTimeslots(){
+        $result = array();
+        $dayArray = explode("/",$this->ScheduleString);
+        foreach($dayArray as $day){
+            $hourArray = explode(",",$day);
+            array_push($result,$hourArray);
+        }
+    }
+}
