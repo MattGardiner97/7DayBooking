a1bbed9aad4ec925fdf0e7a6d683190c4fb46a07
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-04-02 20:35:38 +1000

    Fixed registration bug

--- Website/app/User.php
+++ Website/app/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
 
     protected $attributes = [
         "role" => "Client",
-        "verified" => "false"
+        "verified" => 0
     ];
 
     // Relationships
