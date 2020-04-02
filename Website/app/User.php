04f2781936620605e91e33fd7c76cf613cb70985
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-04-02 16:00:44 +1000

    Added verified column and migration to users

--- Website/app/User.php
+++ Website/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password'
     ];
 
     /**
@@ -38,7 +38,8 @@ class User extends Authenticatable
     ];
 
     protected $attributes = [
-        "role" => "Client"
+        "role" => "Client",
+        "verified" => "false"
     ];
 
     // Relationships
