d1854269a6b762c9702eb9236b1474248588ef34
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-04-03 15:24:24 +1000

    Fixed naming mistake

--- Website/database/migrations/2020_03_26_220639_create_schedule_table.php
+++ Website/database/migrations/2020_03_26_220639_create_schedule_table.php
@@ -29,6 +29,6 @@ class CreateScheduleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Schedule');
+        Schema::dropIfExists('Schedules');
     }
 }
