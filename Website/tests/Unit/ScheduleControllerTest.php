fdd3045e5cf5d767857fbfe3e43f9a8a6c6fe50a
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-04-03 20:29:36 +1000

    Added tests for SchedulesController

--- Website/tests/Unit/ScheduleControllerTest.php
+++ Website/tests/Unit/ScheduleControllerTest.php
@@ -4,14 +4,23 @@ namespace Tests\Unit;
 
 // use PHPUnit\Framework\TestCase;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Schedule;
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use App\User;
 
 class ScheduleControllerTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function CreateCounsellor()
+    {
+        $result = factory(User::class)->create();
+        $result->role = "Counsellor";
+        $result->save();
+        return $result;
+    }
+
     // use RefreshDatabase;
     /**
      * A basic unit test example.
@@ -24,8 +33,58 @@ class ScheduleControllerTest extends TestCase
         $response = $this->get("/schedules/show");
         $response->assertLocation("/login");
 
+        //Ensure users with client role are redirected to home page
         $user = factory(User::class)->create();
         $response = $this->actingAs($user)->get("/schedules/show");
         $response->assertLocation("/");
+
+        //Ensure users with counsellor role are permitted access
+        $user = $this->CreateCounsellor();
+        $response = $this->actingAs($user)->get("/schedules/show");
+        $response->assertOk();
+    }
+
+    public function test_Create()
+    {
+
+        $user = $this->CreateCounsellor();
+        $response = $this->actingAs($user)->get("/schedules/create");
+        $response->assertStatus(302);
+
+    }
+
+    public function test_UpdateGet()
+    {
+        $user = $this->CreateCounsellor();
+        $schedule = factory(Schedule::class)->create();
+        $schedule->CounsellorID = $user->id;
+
+        $response = $this->actingAs($user)->get("/schedules/update?id=" . $schedule->id);
+        $response->assertOk();
+    }
+
+    public function test_UpdatePost(){
+        $user = $this->CreateCounsellor();
+        $schedule = factory(Schedule::class)->create();
+        $schedule->CounsellorID = $user->id;
+
+        $response = $this->actingAs($user)->post("/schedules/update",
+        [
+            "id" => $schedule->id,
+            "CounsellorID" => $user->id,
+            "StartDate" => $schedule->EndDate,
+            "EndDate" => $schedule->StartDate,
+            "ScheduleString" => "10/10/10/10/10"
+        ]);
+
+        $response->assertLocation("/schedules/show");
+    }
+
+    public function test_Delete(){
+        $user = $this->CreateCounsellor();
+        $schedule = factory(Schedule::class)->create();
+        
+        $response = $this->actingAs($user)->get("/schedules/delete?id=" . $schedule->id);
+        $response->assertLocation("/schedules/show");
     }
 }
