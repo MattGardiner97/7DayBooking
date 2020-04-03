b9aeafaac6e227a049f009f4e869e242f07d206b
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-04-03 15:24:12 +1000

    Removed old column

--- Website/database/factories/UserFactory.php
+++ Website/database/factories/UserFactory.php
@@ -25,6 +25,6 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         "role" => "Client",
-        "verified" => 0
+        "requested_verification" => 0
     ];
 });
