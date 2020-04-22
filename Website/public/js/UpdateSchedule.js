cbc36c2b532cacfd7584b486574c503de00b2899
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-04-22 17:14:37 +1000

    Added client-side validation to scheduling views

--- Website/public/js/UpdateSchedule.js
+++ Website/public/js/UpdateSchedule.js
@@ -1,5 +1,10 @@
 function timeslotCell_MouseUp(sender) {
-    SwapState(sender);
+    SwapState(sender, true)
+}
+
+function checkbox_MouseUp(event) {
+    SwapState($(event.target).parent, false);
+    event.stopPropagation();
 }
 
 function BuildScheduleString(Schedule) {
@@ -10,12 +15,19 @@ function BuildScheduleString(Schedule) {
     return dayStrings.join("/");
 }
 
-function SwapState(Cell) {
-    if($(Cell).children("input").prop("checked") == true){
-        $(Cell).children("input").prop("checked",false);
+function SwapState(Cell, SwapCheckboxState = true) {
+    if ($(Cell).children("input").prop("checked") == true) {
+        if (SwapCheckboxState == true) {
+            $(Cell).children("input").prop("checked", false);
+        }
+        $(Cell).removeClass("timeslotAvailable");
+
     }
-    else{
-        $(Cell).children("input").prop("checked",true);
+    else {
+        if (SwapCheckboxState == true) {
+            $(Cell).children("input").prop("checked", true);
+        }
+        $(Cell).addClass("timeslotAvailable");
 
     }
 }
@@ -25,8 +37,8 @@ function Submit_Clicked() {
     var schedule = [[], [], [], [], []];
 
     $(".timeCheckbox").each(function (index, cell) {
-if($(cell).prop("checked") == false)
-return;
+        if ($(cell).prop("checked") == false)
+            return;
 
         var dayIndex = $(cell).parent().attr("data-dayIndex");
         var hour = $(cell).parent().attr("data-hour");
@@ -47,14 +59,58 @@ function DisplayExistingValues(ScheduleString) {
                 return;
             var selector = ".timeslot[data-dayindex=" + i + "][data-hour=" + hour + "]";
             var cell = $(selector);
-            SwapState(cell);
+            SwapState(cell, true);
         });
 
     }
 }
 
+function ValidateDates(){
+    var date = new Date($("#DateStart").val());
+    if (validateDate(date,1) == false) {
+        $("#StartDateErrorMessage").removeClass("d-none");
+        $("#DateStart").addClass("border-danger");
+    }
+    else{
+        $("#StartDateErrorMessage").addClass("d-none");
+        $("#DateStart").removeClass("border-danger");        
+    }
+     date = new Date($("#DateEnd").val());
+    if (validateDate(date,5) == false) {
+        $("#EndDateErrorMessage").removeClass("d-none");
+        $("#DateEnd").addClass("border-danger");   
+    }
+    else{
+        $("#EndDateErrorMessage").addClass("d-none");
+        $("#DateEnd").removeClass("border-danger");        
+    }
+
+    if($("#StartDateErrorMessage").hasClass("d-none") == false|| $("#EndDateErrorMessage").hasClass("d-none") == false){
+        $("#FormSubmit").attr("disabled","").css("cursor","no-drop");
+    }
+    else{
+        $("#FormSubmit").removeAttr("disabled").css("cursor","");
+    }
+}
+
+function startDate_valueChanged(event) {
+    ValidateDates();
+}
+
+function endDate_valueChanged(event) {
+    ValidateDates();    
+}
+
+
+function validateDate(date, expectedDayOfWeek) {
+    if (date.getDay() == expectedDayOfWeek)
+        return true;
+    return false;
+}
+
 $(document).ready(function () {
     var scheduleString = $("#txtSchedule").val();
     DisplayExistingValues(scheduleString);
+    ValidateDates();    
 });
 
