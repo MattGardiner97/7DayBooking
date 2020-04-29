9c4bcafd0ac4d3f2b623224be4282dadad0a4c3b
ERC Software <ercsoftware11@gmail.com>
2020-04-29 15:29:52 +1000

    fix search form

--- Website/resources/views/users/search.blade.php
+++ Website/resources/views/users/search.blade.php
@@ -12,7 +12,7 @@
             <form method='post' action='/users/search'>
                 <label class="form-label text-left"><span class="fa fa-info-circle" data-placement="left"></span>Terms</label>
                 <input type="text" class="form-control" name="search" id="name" value="">
-                <button class="btn btn-sm btn-soft-success mt-5" type="button" name="Search" value="Submit">Search</button>
+                <button class="btn btn-sm btn-soft-success mt-5" type="submit" name="Search" value="Submit">Search</button>
             </form>
         </div>
     </div>
