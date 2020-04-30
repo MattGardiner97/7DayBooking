e0aa837bd21726b04802bf4547f27716f4428141
Mason <larco9mason@gmail.com>
2020-04-30 12:19:06 +1000

    UserController Test 1.0

--- Website/tests/Unit/UserControllerTest.php
+++ Website/tests/Unit/UserControllerTest.php
@@ -18,25 +18,78 @@ class UserControllerTest extends TestCase
 
     public function testCreateUser()
     {
-        $client = factory(User::class)->create();
-        $client->role = "Client";
+        $client = $this->client();
         $this->assertTrue($client->save(), "Couldn't create client.");
+        $response = $this->actingAs($client)->get('/appointments/new');
+        $response->assertLocation('/');
 
-        $counsellor = factory(User::class)->create();
-        $counsellor->role = "Counsellor";
+        $counsellor = $this->counsellor();
         $this->assertTrue($counsellor->save(), "Couldn't create Counsellor.");
+        $response = $this->actingAs($counsellor)->get('/schedules/show');
+        $response->assertLocation('/');
 
-        $admin = factory(User::class)->create();
-        $admin->role = "Admin";
+        $admin = $this->admin();
         $this->assertTrue($admin->save(), "Couldn't create Admin.");
-
-        $result = "Client: " . $client . " Counsellor: " . $counsellor . " Admin: " . $admin;
-        
-        return $result;
+        $response = $this->actingAs($admin)->get('/admin/verify');
+        $response->assertLocation('/');
     }
 
     public function testUpdateUserDetails()
     {
+        $this->withoutExceptionHandling();
 
+        $client = $this->client();
+        $this->assertTrue($client->save(), "Couldn't create client.");
+        $response = $this->actingAs($client)->get('/users/profile');
+        $response->assertLocation('/');
+        $this->actingAs($this->client())->patch('/users/update/{user}', $this->data());
+        $response = $this->patch('/users/update/{user}', [
+            'id' => $client->id, 'name' => 'TESTNAME', 'email' => 'TESTEMAIL@EMAIL.COM', 'password' => 'PASSWORD', 'biography' => ''
+        ]);
+
+
+
+        $counsellor = $this->counsellor();
+        $this->assertTrue($counsellor->save(), "Couldn't create Counsellor.");
+        $response = $this->actingAs($counsellor)->get('/users/profile');
+        $response->assertLocation('/');
+        $this->actingAs($this->client())->patch('/users/update/{user}', $this->data());
+        $response = $this->patch('/users/update/{user}', [
+            'id' => $counsellor->id, 'name' => 'TESTNAME', 'email' => 'TESTEMAIL@EMAIL.COM', 'password' => 'PASSWORD', 'biography' => 'TEST BIOGRAPHY'
+        ]);
     }
+
+    private function client()
+    {
+        $user = factory(User::class)->create();
+        return $user;
+    }
+
+    private function counsellor()
+    {
+        $user = factory(User::class)->create();
+        $user->role = 'Counsellor';
+        $user->save();
+        return $user;
+    }
+
+    private function admin()
+    {
+        $user = factory(User::class)->create();
+        $user->role = 'Admin';
+        $user->save();
+        return $user;
+    }
+
+    private function data()
+    {
+        return [
+            'id' => '1',
+            'name' => '1',
+            'email' => '1',
+            'password' => '2020-01-01',
+            'biography' => '10',
+        ];
+    }
+    
 }
