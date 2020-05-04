64e802e7834880691f1128ee6e482012b93fa59c
ERC Software <ercsoftware11@gmail.com>
2020-05-04 14:00:49 +1000

    Update AppointmentTest.php

--- Website/tests/Unit/AppointmentTest.php
+++ Website/tests/Unit/AppointmentTest.php
@@ -26,28 +26,28 @@ class AppointmentTest extends TestCase
 
     public function test_an_appointment_must_have_a_client()
     {
-        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), [ 'client_id' => '']));
+        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), ['client_id' => '']));
 
         $response->assertSessionHasErrors('client_id');
     }
 
     public function test_an_appointment_must_have_a_counsellor()
     {
-        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), [ 'counsellor_id' => '']));
+        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), ['counsellor_id' => '']));
 
         $response->assertSessionHasErrors('counsellor_id');
     }
 
     public function test_an_appointment_must_have_a_date()
     {
-        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), [ 'date' => '']));
+        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), ['date' => '']));
 
         $response->assertSessionHasErrors('date');
     }
 
     public function test_an_appointment_must_have_a_time()
     {
-        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), [ 'time' => '']));
+        $response = $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), ['time' => '']));
 
         $response->assertSessionHasErrors('time');
     }
@@ -81,15 +81,86 @@ class AppointmentTest extends TestCase
         $response->assertViewIs('appointments.changed');
     }
 
-    public function test_error_when_user_not_logged_in_tries_to_create_appointment()
+    public function test_fail_when_user_not_logged_in_tries_to_create_appointment()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->post('/appointments', $this->data());
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function test_can_get_clients_appointments_and_no_one_elses()
+    {
+        $client = $this->client();
+
+        $counsellor = $this->counsellor();
+
+        $this->actingAs($counsellor)->post('/appointments', array_merge($this->data(), ['client_id' => $client->id]));
+        $this->actingAs($counsellor)->post('/appointments', array_merge($this->data(), ['client_id' => $client->id, 'time' => '11']));
+
+        $this->actingAs($counsellor)->post('/appointments', array_merge($this->data(), ['client_id' => 5, 'time' => '06']));
+        $this->actingAs($counsellor)->post('/appointments', array_merge($this->data(), ['client_id' => 6, 'time' => '07']));
+
+        $this->assertCount(4, Appointment::all());
+
+        $response = $this->actingAs($client)->get('/appointments/show');
+
+        $appointments = $this->getResponseData($response, 'appointments');
+
+        $this->assertCount(2, $appointments);   
+    }
+
+    public function test_notes_can_be_null()
+    {
+        $this->actingAs($this->client())->post('/appointments', array_merge($this->data(), ['notes' => '']));
+
+        $this->assertCount(1, Appointment::all());
+
+        $appointment = Appointment::first();
+
+        $this->assertEquals(null, $appointment->notes);
+    }  
+
+    public function test_a_counsellor_cannot_access_an_appointment_that_does_not_belong_to_them()
+    {
+        $client = $this->client();
+        $counsellor = $this->counsellor();
+
+        $this->actingAs($client)->post('/appointments', array_merge($this->data(), ['counesllor_id' => $counsellor->id]));
+
+        $this->actingAs($client)->post('/appointments', array_merge($this->data(), ['counesllor_id' => 3, 'time' => 5]));
+
+        $this->assertCount(2, Appointment::all());
+
+        $response = $this->actingAs($counsellor)->get('/appointments/edit/' . Appointment::where('id', 1)->first()->id);
+        $response->assertOk();
+
+        $response = $this->actingAs($counsellor)->get('/appointments/edit/' . Appointment::where('id', 2)->first()->id);
         
         $response->assertStatus(302);
         $response->assertRedirect('/login');
     }
+
+    public function test_a_client_cannot_access_an_appointment_that_does_not_belong_to_them()
+    {
+        $client = $this->client();
+
+        $counsellor = $this->counsellor();
+
+        $this->actingAs($counsellor)->post('/appointments', array_merge($this->data(), ['client_id' => $client->id]));
+
+        $this->actingAs($counsellor)->post('/appointments', array_merge($this->data(), ['client_id' => 3, 'time' => 1]));
+
+        $this->assertCount(2, Appointment::all());
+
+        $response = $this->actingAs($client)->get('/appointments/edit/' . Appointment::where('id', 1)->first()->id);
+        $response->assertOk();
+
+        $response = $this->actingAs($client)->get('/appointments/edit/' . Appointment::where('id', 2)->first()->id);
+        
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }    
 
     private function data()
     {
@@ -99,6 +170,7 @@ class AppointmentTest extends TestCase
             'client_id' => '1',
             'date' => '2020-01-01',
             'time' => '10',
+            'notes' => 'Test',
         ];
     }
 
@@ -114,5 +186,14 @@ class AppointmentTest extends TestCase
     {
         $user = factory(User::class)->create();
         return $user;
+    }
+
+    private function getResponseData($response, $key)
+    {
+        $content = $response->getOriginalContent();
+
+        $content = $content->getData();
+
+        return $content[$key]->all();
     }
 }
