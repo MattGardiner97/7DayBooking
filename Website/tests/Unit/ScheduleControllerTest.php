ad8932f96a86fab4209ebef4331fb995729334b7
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-05-06 17:34:35 +1000

    Added more tests to ScheduleControllerTests

--- Website/tests/Unit/ScheduleControllerTest.php
+++ Website/tests/Unit/ScheduleControllerTest.php
@@ -21,11 +21,17 @@ class ScheduleControllerTest extends TestCase
         return $result;
     }
 
-    // use RefreshDatabase;
+    public function CreateClient()
+    {
+        $result = factory(User::class)->create();
+        $result->role = "Client";
+        $result->save();
+        return $result;
+    }
+
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * Test show all schedules for logged in user
+     * Endpoint: GET /schedules/show
      */
     public function test_Show()
     {
@@ -34,7 +40,7 @@ class ScheduleControllerTest extends TestCase
         $response->assertLocation("/login");
 
         //Ensure users with client role are redirected to home page
-        $user = factory(User::class)->create();
+        $user = $this->CreateClient();
         $response = $this->actingAs($user)->get("/schedules/show");
         $response->assertLocation("/");
 
@@ -44,56 +50,162 @@ class ScheduleControllerTest extends TestCase
         $response->assertOk();
     }
 
+    /**
+     * Test Create schedule for logged in user
+     * Endpoint: POST /schedules/create
+     */
     public function test_Create()
     {
-
-        $user = $this->CreateCounsellor();
-        $response = $this->actingAs($user)->post("/schedules/create",[
+        //Test as client
+        $user = $this->CreateClient();
+        $response = $this->actingAs($user)->post("/schedules/create", [
             "startDate" => "2020-4-8",
-            "endDate" => "2021-4-8"
+            "endDate" => "2021-4-8",
+        ]);
+        $response->assertLocation("/");
+
+        //Test as counsellor with valid form
+        $user = $this->CreateCounsellor();
+        $response = $this->actingAs($user)->post("/schedules/create", [
+            "startDate" => "2020-4-8",
+            "endDate" => "2021-4-8",
         ]);
         $response->assertStatus(302);
 
+        //Test as counsellor with bad form
+        $response = $this->actingAs($user)->post("/schedules/create", [
+            "stringfield" => "hello world",
+            "endDate" => "2021-4-8",
+        ]);
+        $response->assertSessionHasErrors(["startDate"]);
+
     }
 
-    public function test_New(){
+    /**
+     * Test New schedule page
+     * Endpoint: GET /schedules/new
+     */
+    public function test_New()
+    {
+        //Test as client
+        $user = $this->CreateClient();
+        $response = $this->actingAs($user)->get("/schedules/new");
+        $response->assertLocation("/");
+
+        //Test as valid user
         $user = $this->CreateCounsellor();
         $response = $this->actingAs($user)->get("/schedules/new");
         $response->assertOk();
+        $response->assertViewIs("schedules.new");
     }
 
+    /**
+     * Test update page
+     * Endpoint: GET /schedules/update
+     */
     public function test_UpdateGet()
     {
-        $user = $this->CreateCounsellor();
         $schedule = factory(Schedule::class)->create();
-        $schedule->CounsellorID = $user->id;
 
+        //Test as client
+        $user = $this->CreateClient();
+        $response = $this->actingAs($user)->get("/schedules/update?id=" . $schedule->id);
+        $response->assertLocation("/");
+
+        //Test as counsellor
+        $user = $this->CreateCounsellor();
+        $schedule->CounsellorID = $user->id;
+        $schedule->save();
         $response = $this->actingAs($user)->get("/schedules/update?id=" . $schedule->id);
         $response->assertOk();
+        
+        //Test counsellor cannot edit another counsellors schedule
+        $user2 = $this->CreateCounsellor();
+        $response = $this->actingAs($user2)->get("/schedules/update?id=" . $schedule->id);
+        $response->assertStatus(403);
     }
 
-    public function test_UpdatePost(){
-        $user = $this->CreateCounsellor();
+    /**
+     * Test update endpoint
+     * Endpoint: POST /schedules/update
+     */
+    public function test_UpdatePost()
+    {        
         $schedule = factory(Schedule::class)->create();
+
+        //Test as client
+        $user = $this->CreateClient();
+        $response = $this->actingAs($user)->post("/schedules/update",
+            [
+                "id" => $schedule->id,
+                "StartDate" => $schedule->EndDate,
+                "EndDate" => $schedule->StartDate,
+                "ScheduleString" => "10/10/10/10/10",
+            ]);
+        $response->assertLocation("/");
+
+        //Test as counsellor
+        $user = $this->CreateCounsellor();
         $schedule->CounsellorID = $user->id;
+        $schedule->save();
 
         $response = $this->actingAs($user)->post("/schedules/update",
-        [
-            "id" => $schedule->id,
-            "CounsellorID" => $user->id,
-            "StartDate" => $schedule->EndDate,
-            "EndDate" => $schedule->StartDate,
-            "ScheduleString" => "10/10/10/10/10"
-        ]);
-
+            [
+                "id" => $schedule->id,
+                "StartDate" => $schedule->EndDate,
+                "EndDate" => $schedule->StartDate,
+                "ScheduleString" => "10/10/10/10/10",
+            ]);
         $response->assertLocation("/schedules/show");
+
+        //Test invalid paramaters
+        $response = $this->actingAs($user)->post("/schedules/update",
+            [
+                "id" => "hello world",
+                "StartDate" => $schedule->EndDate,
+                "EndDate" => $schedule->StartDate,
+                "ScheduleString" => "hello world",
+            ]);
+            $response->assertSessionHasErrors(["id", "ScheduleString"]);
+
+        //Ensure counsellors cannot edit another counsellors schedule
+        $user2 = $this->CreateCounsellor();
+        $response = $this->actingAs($user2)->post("/schedules/update",
+            [
+                "id" => $schedule->id,
+                "StartDate" => $schedule->EndDate,
+                "EndDate" => $schedule->StartDate,
+                "ScheduleString" => "10/10/10/10/10",
+            ]);
+        $response->assertStatus(403);
     }
 
-    public function test_Delete(){
-        $user = $this->CreateCounsellor();
+    /**
+     * Test delete endpoint
+     * Endpoint: POST /schedules/delete
+     */
+    public function test_Delete()
+    {
         $schedule = factory(Schedule::class)->create();
-        
+
+        //Test as client
+        $user = $this->CreateClient();
+        $response = $this->actingAs($user)->get("/schedules/delete?id=" . $schedule->id);
+        $response->assertLocation("/");
+
+        //Test as counsellor
+        $user = $this->CreateCounsellor();
+        $schedule->CounsellorID = $user->id;
+        $schedule->save();
         $response = $this->actingAs($user)->get("/schedules/delete?id=" . $schedule->id);
         $response->assertLocation("/schedules/show");
+
+        //Test as another counsellor
+        $schedule = factory(Schedule::class)->create();
+        $schedule->CounsellorID = $user->id;
+        $schedule->save();
+        $user2 = $this->CreateCounsellor();
+        $response = $this->actingAs($user2)->get("/schedules/delete?id=" . $schedule->id);
+        $response->assertStatus(403);
     }
 }
