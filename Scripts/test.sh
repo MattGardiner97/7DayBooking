a67eaa9f618e2988731462803005773b6b370141
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-05-12 16:41:20 +1000

    Update test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,3 +1,3 @@
 set -e
 
-php /home/mattgardiner/staging/Website/vendor/bin/phpunit -c /home/mattgardiner/staging/Website/phpunit.xml /home/mattgardiner/staging/Website/tests/Unit
+php /home/mattgardiner/staging/Website/vendor/bin/phpunit -c /home/mattgardiner/staging/Website/phpunit.xml --coverage-html /home/mattgardiner/githook/coverage --testdox-html /home/mattgardiner/githook/testresults.html --whitelist /home/mattgardiner/staging/Website/app /home/mattgardiner/staging/Website/tests/Unit > /home/mattgardiner/gitook/phpunit.log
