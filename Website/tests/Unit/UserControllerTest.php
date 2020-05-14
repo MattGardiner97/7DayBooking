e0309f40b059c21feadf274c3d92c67d98947f15
Mason <larco9mason@gmail.com>
2020-05-14 13:53:18 +1000

    UserControllerTest Updates

--- Website/tests/Unit/UserControllerTest.php
+++ Website/tests/Unit/UserControllerTest.php
@@ -5,75 +5,81 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
-use Illuminate\Support\Facades\Hash;
 
 class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testCreateUser()
+    public function testCreateClient()
     {
         $client = $this->client();
         $this->assertTrue($client->save(), "Couldn't create client.");
         $response = $this->actingAs($client)->get('/appointments/new');
-        $response->assertLocation('/');
+        $response->assertLocation('/');        
+    }
 
+    public function testCreateCounsellor()
+    {
         $counsellor = $this->counsellor();
         $this->assertTrue($counsellor->save(), "Couldn't create Counsellor.");
         $response = $this->actingAs($counsellor)->get('/schedules/show');
         $response->assertLocation('/');
+    }
 
+    public function testCreateAdmin()
+    {
         $admin = $this->admin();
         $this->assertTrue($admin->save(), "Couldn't create Admin.");
         $response = $this->actingAs($admin)->get('/admin/verify');
         $response->assertLocation('/');
     }
 
-    public function testUpdateUserDetails()
+    public function testUpdateClientDetails()
     {
         $response = $this->actingAs($this->client())->post('/users/update', $this->clientData());
         $client = User::where('id', '1')->first();
         $this->assertEquals('clientName', $client->name);
         $this->assertEquals('client@email.com', $client->email);
-        $this->assertEquals('clientBiography', $client->biography);
+        $this->assertEquals('clientBiography', $client->biography);  
+    }
 
-        $response = $this->actingAs($this->client())->post('/users/update', $this->counsellorData());
-        $counsellor = User::where('id', '2')->first();
+    public function testUpdateCounsellorDetails()
+    {
+        $response = $this->actingAs($this->counsellor())->post('/users/update', $this->counsellorData());
+        $counsellor = User::where('id', '1')->first();
         $this->assertEquals('counsellorName', $counsellor->name);
         $this->assertEquals('counsellor@email.com', $counsellor->email);
         $this->assertEquals('counsellorBiography', $counsellor->biography);
     }
 
-    public function testShowAllCounsellors()
+    public function testClientViewAllCounsellors()
     {
         $client = $this->client();
-        $counsellor = $this->counsellor();
-
         $response = $this->actingAs($client)->get('/users/list');
         $response->assertLocation('/');
+    }
+
+    public function testCounsellorViewAllCounsellors()
+    {
+        $counsellor = $this->counsellor();
         $response = $this->actingAs($counsellor)->get('/users/list');
         $response->assertLocation('/');
     }
 
-    public function testSearchByResults()
+    public function testClientViewSearchedResults()
     {
         $counsellor = $this->counsellor();
-        $response = $this->actingAs($this->client())->withSession(['search' => $counsellor->name])->post('/users/search');
+        $response = $this->actingAs($this->client())->post('/users/search', ['search' => $counsellor->name]);
         $response->assertSessionHas('search', $counsellor->name);
     }
-    //Change to another assert?
-    public function testSearchBy()
+
+    public function testCounsellorViewSearchedResults()
     {
-        $response = $this->actingAs($this->client())->get('/users/search');
-        $response->assertLocation('/');
+        $counsellor = $this->counsellor();
+        $response = $this->actingAs($this->counsellor())->post('/users/search', ['search' => $counsellor->name]);
+        $response->assertSessionHas('search', $counsellor->name);
     }
-    //Change to another assert?
-    public function testShow()
-    {
-        $response = $this->actingAs($this->counsellor())->get('/users/show/{user}', $this->counsellorData());
-        $response->assertLocation('/');
-    }
-    
+
     private function client()
     {
         $client = factory(User::class)->create();
@@ -112,7 +118,7 @@ class UserControllerTest extends TestCase
     private function counsellorData()
     {
         return [
-            'id' => '2',
+            'id' => '1',
             'name' => 'counsellorName',
             'email' => 'counsellor@email.com',
             'password' => 'counsellorPassword',
